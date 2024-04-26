09f97f5d5f1bf7f01617cf6dccc9ebde84d49374
Brycycle <44349959+Bryce-Davidson@users.noreply.github.com>
2024-04-26 11:40:14 -0700

    feat: add parser

--- GameParser.py
+++ GameParser.py
@@ -8,25 +8,40 @@ class GameParser:
     def __init__(self, input_path, output_path):
         self.input_path = input_path
         self.output_path = output_path
-        self.data = []
+        self.input = None
+        self.output = None
+
+    def read(self):
+        with open(self.input_path, "r") as f:
+            self.input = f.readlines()
 
     def parse(self):
-        with open(self.input_path, "r") as f:
-            for line in f:
-                json_object = json.loads(line)
-                self.data.append(json_object)
+        self.output = []
+        for line in self.input:
+            json_object = json.loads(line)
+            self.output.append(json_object)
 
-    def write(self):
-        with open(self.output_path, "w") as f:
-            json.dump(self.data, f)
+    def to_json(self):
+        with open(f"{self.output_path}/output.json", "w") as f:
+            json.dump(self.output, f)
 
     def normalize(self):
-        self.data = pd.json_normalize(self.data)
+        self.output = pd.json_normalize(self.output)
+
+    def write(self):
+        self.normalize()
+        self.output.to_csv(f"{self.output_path}/output.csv", index=False)
 
 
 if __name__ == "__main__":
     input_path = "./data/out.log"
-    output_path = "./data/out.json"
+    output_path = "./data"
     parser = GameParser(input_path, output_path)
 
+    parser.read()
     parser.parse()
+    parser.to_json()
+    parser.write()
+
+    # df = pd.read_csv(f"{output_path}/output.csv")
+    # print(df.columns)
